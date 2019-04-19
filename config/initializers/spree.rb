0c1cedd0d1af9b861766b7d79e0c8103bf4f37b7
brnaldomesi <brnaldomesi@yandex.com>
2019-04-19 11:37:48 -0400

    Add Image for Logo
    Logo is mis-sized need to learn how to add decorators

--- config/initializers/spree.rb
+++ config/initializers/spree.rb
@@ -22,7 +22,7 @@ Spree.config do |config|
   # Frontend:
 
   # Custom logo for the frontend
-  config.logo = "logo/tbdmarket_logo.png"
+  config.logo = "logo/logo_tbd_market.png"
 
   # Template to use when rendering layout
   # config.layout = "spree/layouts/spree_application"
