1cf7a785dac6c79a4fb2801c7d2110dd41242114
brnaldomesi <brnaldomesi1@gmail.com>
2019-04-24 14:15:02 -0400

    Products: fixed SellerUser permissions.  Edit form changes: removed slug field, changed Category and Shipping to default

--- app/models/spree/permission_sets/seller_user.rb
+++ app/models/spree/permission_sets/seller_user.rb
@@ -10,6 +10,7 @@ module Spree
         ::Spree::ProductUserRelation::MANAGEABLE_CLASSES.each do|klass|
           next unless klass.new.respond_to?(:user_id)
           can :manage, klass, user_id: user.id
+          can [:new, :create], klass
         end
 
         # These don't have ref to product
@@ -17,6 +18,7 @@ module Spree
 
         # TODO: somehow the looped 'can :manage' calls don't work
         can :manage, Spree::Product, user_id: user.id
+        can [:new, :create], Spree::Product
         can :manage, Spree::OptionValue
         can :manage, Spree::StockItem
         can :manage, Spree::StockLocation
