650ca45b04e0aafbcdf4237953bdf39f9292250e
brnaldomesi <brnaldomesi@yandex.com>
2019-04-24 14:15:02 -0400

    Products: fixed SellerUser permissions.  Edit form changes: removed slug field, changed Category and Shipping to default

--- config/initializers/models/spree/admin/products_controller.rb
+++ config/initializers/models/spree/admin/products_controller.rb
@@ -8,6 +8,22 @@ module Spree
         @new.user_id = spree_current_user.try(:id) if @new
       end
 
+      ##
+      # Copied over code and then added w/ extra current user only condition.
+      def collection
+        return @collection if @collection
+        params[:q] ||= {}
+        params[:q][:s] ||= "name asc"
+        # @search needs to be defined as this is passed to search_form_for
+        @search = super.ransack(params[:q])
+        @collection = @search.result.
+            order(id: :asc).
+            includes(product_includes).
+            page(params[:page]).
+            per(Spree::Config[:admin_products_per_page])
+        @collection = @collection.where(user_id: spree_current_user.try(:id) ) if spree_current_user
+      end
+
     end # eval
   end
 end
