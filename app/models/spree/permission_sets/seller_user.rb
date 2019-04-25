5dc26f22afb2302c5407f0f013664e40affe8bc7
brnaldomesi <brnaldomesi1@gmail.com>
2019-04-25 10:51:01 -0400

    Products: fixed settings for Price to resolve permissions and forms

--- app/models/spree/permission_sets/seller_user.rb
+++ app/models/spree/permission_sets/seller_user.rb
@@ -10,15 +10,16 @@ module Spree
         ::Spree::ProductUserRelation::MANAGEABLE_CLASSES.each do|klass|
           next unless klass.new.respond_to?(:user_id)
           can :manage, klass, user_id: user.id
-          can [:new, :index, :create], klass
+          can [:new, :create], klass
         end
 
         # These don't have ref to product
+        can :manage, Spree::Price
         can :manage, Spree::Image
 
         # TODO: somehow the looped 'can :manage' calls don't work
         can :manage, Spree::Product, user_id: user.id
-        can [:new, :index, :create], Spree::Product
+        can [:new, :create], Spree::Product
         can :manage, Spree::OptionValue
         can :manage, Spree::StockItem
         can :manage, Spree::StockLocation
