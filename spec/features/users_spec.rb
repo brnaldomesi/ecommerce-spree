2e534f9b3452cf0365b23020914765c756f6e781
brnaldomesi <brnaldomesi@yandex.com>
2019-04-26 12:08:00 -0400

    Users: added username and display_name columns to spree_users.  Added fields accordingly to registration form.  Change login form to allow username on top of email-only

---
+++ spec/features/users_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+require 'shared/users_spec_helper'
+
+include UsersSpecHelper
+
+RSpec.describe 'Register a user', type: :feature do
+  # routes { Spree::Core::Engine.routes }
+
+  context 'Registering user' do
+    let :user_attr do
+      attributes_for(:basic_user)
+    end
+    let(:email) { user_attr[:email] }
+    let(:username) { user_attr[:username] }
+    let(:display_name) { user_attr[:display_name] }
+
+    it 'Sign up' do
+      puts '---- Sign up user'
+      user = sign_up_with(user_attr[:email], 'test1234', user_attr[:username], user_attr[:display_name] )
+
+      check_user_abilities(user)
+
+    end
+  end
+
+end
