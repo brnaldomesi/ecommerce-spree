6ef126e68bfa0d5f16bd4803e9ccd4b9396b130c
brnaldomesi <brnaldomesi@yandex.com>
2019-04-30 09:28:11 -0400

    Server: added mailer settings; added use of Redis, although holding back from using Redis as cache_store

--- config/application.rb
+++ config/application.rb
@@ -2,6 +2,8 @@ require_relative 'boot'
 
 require 'rails/all'
 
+require_relative 'initializers/redis'
+
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -31,5 +33,8 @@ module SolidusMarket
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+
+    # Redis
+    # config.cache_store = :redis_store, REDIS_CONFIG
   end
 end
