e77e3967278665df13ce44186b2e3baafdd8c618
brnaldomesi <brnaldomesi@yandex.com>
2019-06-05 16:53:31 -0400

    Products: Variants: added test of access to variants

--- spec/factories/users_factory.rb
+++ spec/factories/users_factory.rb
@@ -11,6 +11,12 @@ FactoryBot.define do
       display_name { 'Bison' }
     end
 
+    factory :another_user, aliases: [:viewer_2, :buyer_2, :boy_user_2] do
+      email { 'buyer02@gmail.com' }
+      username { 'connor' }
+      display_name { 'Connor' }
+    end
+
     factory :seller, aliases: [:basic_seller, :girl_user] do
       email { 'seller01@gmail.com' }
       username { 'sally' }
