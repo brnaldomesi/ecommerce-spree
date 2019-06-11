094f7d603f3f6d4f1e14de676c4f2846d7fd9ca2
brnaldomesi <brnaldomesi@yandex.com>
2019-06-11 14:05:22 -0400

    Deploy: fixed list of keys needed as separate GIT one is needed

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -7,7 +7,7 @@ server 'tbdmarket.com', user: 'deploy', roles: %w{web app db}
 
 set :ssh_options, {
     user: 'deploy',
-    keys: ['~/.ssh/tbdmarket'],
+    keys: ['~/.ssh/tbdmarket', '~/.ssh/id_rsa'],
     forward_agent: true,
     auth_methods: %w(publickey)
 }
