33bb50f776f988b7953227e71e3b13c5d2aa5d58
brnaldomesi <brnaldomesi1@gmail.com>
2019-06-12 15:59:36 -0400

    Deploy: turned off staging environment's eager_loading as trial to fix error

--- config/deploy.rb
+++ config/deploy.rb
@@ -76,6 +76,17 @@ set :puma_init_active_record, false  # Change to true if using ActiveRecord
 #######################################
 # Extra actions
 
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/sockets -p"
+      execute "mkdir #{shared_path}/pids -p"
+    end
+  end
+
+  before :deploy, :make_dirs
+end
 
 namespace :deploy do
   desc 'Initial Deploy'
