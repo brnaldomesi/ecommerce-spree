c13e87347778cfa1a72fb401e163152cf2d78a4c
brnaldomesi <brnaldomesi@yandex.com>
2019-06-14 10:20:10 -0400

    DB: fixed admin user's needed admin role

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_13_201703) do
+ActiveRecord::Schema.define(version: 2019_06_14_141304) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name", null: false
