ce7eb3a635593c7ae828206b3b4b5b78bc315196
brnaldomesi <brnaldomesi1@gmail.com>
2019-06-21 11:59:18 -0400

    Products: fixed OptionType sanitize method

--- app/models/spree/option_type_decorator.rb
+++ app/models/spree/option_type_decorator.rb
@@ -4,8 +4,8 @@ module Spree
 
     VALID_OPTION_NAME_REGEX = /^((max|maximum|minmum|min|average|avg|estimate|est|US|UK|original|market|man'?s?|men'?s?|woman'?s?|women'?s?|kids?|adults?|new|supported|product|item)\s+)?([a-z]{2,}\s+)?(types?|category|name|colou?rs?|sizes?|number|no|sku|brand|width|length|height|diameter|radius|thickness|area|waist|bust|collar|weight|depth|models?|material|fabric|quality|quantity|count|pieces?|style|group|range|age|gender|level|class|capacity|time|date|life|season|quantity|version|edition|mode|port|payment|pattern|price|cost|charge|fee|rate|frequency|response|speed|bandwidth|volume|shape|method|current|voltage|percentage|ratio|frequency|condition|code|sleeve|sensitivity|grade|rating|platform|protocol|operating\s+system|format|angle|interface|standard)$/i
 
-    def self.valid_option_name?(name)
-      !sanitize_keyword_name(name).match(VALID_OPTION_NAME_REGEX).nil?
+    def self.valid_option_name?(name = '')
+      !name.to_sanitize_keyword_name.match(VALID_OPTION_NAME_REGEX).nil?
     end
 
     protected
