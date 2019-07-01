2c6a9d4ad6f7e8b1f0b0663e1e943e4c48709180
brnaldomesi <brnaldomesi1@gmail.com>
2019-07-01 14:33:18 -0400

    Products: added alternative to build a clone of master product

---
+++ lib/spree/product_duplicator_decorator.rb
@@ -0,0 +1,23 @@
+module Spree
+  ProductDuplicator.class_eval do
+    ##
+    # Changed version of Spree::ProductDuplicator#duplicate w/o saving files.
+    def build_clone(&block)
+      draft = product.dup.tap do |new_product|
+        new_product.taxons = product.taxons
+        new_product.created_at = nil
+        new_product.deleted_at = nil
+        new_product.updated_at = nil
+        new_product.product_properties = reset_properties
+        new_product.price = product.price
+        # new_product.master = duplicate_master
+        new_product.sku = nil if product.sku.blank?
+      end
+      draft.option_types = product.option_types
+      draft.master_product_id = product.id
+      yield draft if block_given?
+      draft
+    end
+
+  end
+end
