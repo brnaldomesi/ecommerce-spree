e3eda6c127d2cb8edfacaad5fb53e8f41faa5358
brnaldomesi <brnaldomesi@yandex.com>
2019-07-23 09:46:21 -0400

    Products: Option Values: cleaned values of clothing size

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_22_191503) do
+ActiveRecord::Schema.define(version: 2019_07_23_132155) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=latin1", force: :cascade do |t|
     t.string "name", null: false
