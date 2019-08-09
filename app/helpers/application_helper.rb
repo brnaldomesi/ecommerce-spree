9b68ea705489efbe872f7f2ffa7b7afe1bd43d3b
brnaldomesi <brnaldomesi1@gmail.com>
2019-08-09 16:01:11 -0400

    Cart: added method and partial to generate list of cart items and cart link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -11,4 +11,8 @@ module ApplicationHelper
   def title
     ''
   end
+
+  def order_in_cart
+    @order ||= Spree::Order.incomplete.find_or_initialize_by(guest_token: cookies.signed[:guest_token])
+  end
 end
