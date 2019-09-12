3ec68bab58259b621e52df03d71ca10ce508b088
brnaldomesi <brnaldomesi1@gmail.com>
2019-09-12 13:14:13 -0400

    Selling: Accept Methods: added attributes account_parameters and account label

--- app/controllers/spree/store_payment_methods_controller.rb
+++ app/controllers/spree/store_payment_methods_controller.rb
@@ -5,6 +5,8 @@ module Spree
     helper Spree::PaymentMethodsHelper
 
     before_action :load_collection, only: [:index]
+    before_action :set_params, only: [:create, :update]
+    after_action :clear_notice, except: [:index]
     # before_action :load_resource, except: [:index, :create]
 
     def index
@@ -13,20 +15,35 @@ module Spree
     end
 
     def create
-      params.permit!
-      @store_payment_method = ::Spree::StorePaymentMethod.new(params[:store_payment_method])
+      @store_payment_method = ::Spree::StorePaymentMethod.new( resource_params )
       @store_payment_method.store_id = spree_current_user.store.id
       create!(notice: '') { store_payment_methods_path(added_payment_method_id: @store_payment_method.payment_method_id) }
     end
 
+    def update
+      update!(notice: '') { store_payment_methods_path }
+    end
+
     def destroy
       super do|f|
-        flash[:notice] = ''
         f.html { redirect_to store_payment_methods_path(show_other_payment_methods: params[:show_other_payment_methods], deleted_payment_method_id: resource.try(:payment_method_id) ) }
       end
     end
 
     private
+
+    def set_params
+      # If account_parameters is a Hash of multiple inner attributes, convert to JSON
+      account_parameters = params[:store_payment_method][:account_parameters]
+      if account_parameters.is_a?(Hash)
+        params[:store_payment_method][:account_parameters] = account_parameters.to_json
+      end
+      params.permit! # let inherited_resources handle attribute permits
+    end
+
+    def clear_notice
+      flash[:notice] = ''
+    end
 
     def collection
       @collection = ::Spree::StorePaymentMethod.where(store_id: spree_current_user.fetch_store.id).includes(:payment_method).all
