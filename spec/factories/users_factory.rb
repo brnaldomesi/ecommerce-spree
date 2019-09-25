5e41ebf20d5711e492ff5731639228c8590dc0e7
brnaldomesi <brnaldomesi@yandex.com>
2019-09-25 10:16:01 -0400

    Testing: added couple entries of factory records for address, state, and country

--- spec/factories/users_factory.rb
+++ spec/factories/users_factory.rb
@@ -1,3 +1,13 @@
+def find_or_create(factory_key, primary_search_attribute)
+  new_obj = build(factory_key)
+  existing = new_obj.class.where(primary_search_attribute => new_obj.send(primary_search_attribute) ).first
+  unless existing
+    new_obj.save
+    existing = new_obj
+  end
+  existing
+end
+
 FactoryBot.define do
   factory :user, class: Spree::User do
 
@@ -21,6 +31,40 @@ FactoryBot.define do
       email { 'seller01@gmail.com' }
       username { 'sally' }
       display_name { 'Sally' }
+    end
+  end
+
+  factory :address, class: Spree::Address do
+    factory :basic_address, aliases: [:boston, :usa_address, :business_address] do
+      firstname { 'Mary' }
+      lastname { 'Sanders' }
+      address1 { '100 Washington ST' }
+      city { 'Boston' }
+      zipcode { '02161' }
+      phone { '6177208112' }
+      state_name { 'MA' }
+      company { 'MS Merchandise' }
+      country_id { find_or_create(:country_usa, :name).id }
+      state_id { find_or_create(:ma_state, :name).id  }
+    end
+  end
+
+  factory :country, class: Spree::Country do
+    factory :country_usa, aliases: [:basic_country] do
+      iso_name { 'UNITED STATES' }
+      iso { 'US' }
+      iso3 { 'USA' }
+      name { 'United States' }
+      numcode { 840 }
+      states_required { true }
+    end
+  end
+
+  factory :state, class: Spree::State do
+    factory :state_ma, aliases: [:ma_state] do
+      name { 'Massachusetts' }
+      abbr { 'CA' }
+      country_id { find_or_create(:country_usa, :name).id }
     end
   end
 end
