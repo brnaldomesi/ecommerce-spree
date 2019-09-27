bd7d99f38611faf9306276c964b72dd7016fa0a2
brnaldomesi <brnaldomesi@yandex.com>
2019-09-27 13:31:07 -0400

    Orders: added product_id to LineIem so easier to query after completed order operations

--- app/controllers/spree/checkout_controller_decorator.rb
+++ app/controllers/spree/checkout_controller_decorator.rb
@@ -2,39 +2,6 @@
 
   helper Spree::CheckoutHelperExtension
 
-  def update
-    order_update = ::Spree::OrderUpdateAttributes.new(@order, update_params, request_env: request.headers.env)
-    logger.info "| order_update: #{order_update}"
-    logger.info "| update_params: #{update_params.as_json}"
-    # logger.info "| request: #{request.headers.env}" # this is be huge data to show
-
-    if update_order
-
-      assign_temp_address
-      logger.info "  after update_order: #{@order.attributes}"
-      logger.info "  can_complete? #{@order.can_complete?}"
-      logger.info "  payments: #{@order.payments.all.collect(&:attributes)}"
-
-      unless transition_forward
-        redirect_on_failure
-        return
-      end
-      logger.info "  completed? #{@order.completed?}"
-      if @order.completed?
-        finalize_order
-      else
-        logger.info "-> next: #{checkout_state_path(@order.state) }"
-        send_to_next_state
-      end
-
-    else
-      logger.warn "** Canoot update order: #{flash[:error] || flash[:notice]}, order #{@order.errors.full_messages}"
-      render :edit
-    end
-  end
-
-  protected
-
   ##
   # This replaces app's own routes method, and auto adds order_id: @order.id,
   # so @order or params[:order_id] is expected.
